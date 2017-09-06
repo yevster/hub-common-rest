007d11352389cb30dd68ea966dea99ba57e812aa
jrichard <jrichard@blackducksoftware.com>
2017-09-06 11:09:08 -0400

    Fixing minor mistakes

--- src/main/java/com/blackducksoftware/integration/hub/certificate/CertificateHandler.java
+++ src/main/java/com/blackducksoftware/integration/hub/certificate/CertificateHandler.java
@@ -1,13 +1,25 @@
-/*
- * Copyright (C) 2017 Black Duck Software Inc.
- * http://www.blackducksoftware.com/
- * All rights reserved.
+/**
+ * Hub Common Rest
  *
- * This software is the confidential and proprietary information of
- * Black Duck Software ("Confidential Information"). You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Black Duck Software.
+ * Copyright (C) 2017 Black Duck Software, Inc.
+ * http://www.blackducksoftware.com/
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package com.blackducksoftware.integration.hub.certificate;
 
@@ -31,9 +43,7 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 
-import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
@@ -42,7 +52,6 @@ import org.apache.commons.lang3.StringUtils;
 
 import com.blackducksoftware.integration.exception.IntegrationCertificateException;
 import com.blackducksoftware.integration.exception.IntegrationException;
-import com.blackducksoftware.integration.hub.rest.CertTrustManager;
 import com.blackducksoftware.integration.hub.rest.TLSSocketFactory;
 import com.blackducksoftware.integration.log.IntLogger;
 import com.blackducksoftware.integration.util.proxy.ProxyUtil;
@@ -129,14 +138,6 @@ public class CertificateHandler {
             } else {
                 clientBuilder.sslSocketFactory(systemDefaultSslSocketFactory(trustManager), trustManager);
             }
-
-            clientBuilder.hostnameVerifier(new HostnameVerifier() {
-                @Override
-                public boolean verify(final String hostname, final SSLSession session) {
-                    return true;
-                }
-            });
-
             final OkHttpClient client = clientBuilder.build();
 
             final HttpUrl.Builder urlBuilder = HttpUrl.get(url).newBuilder();
